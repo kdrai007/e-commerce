620103eac037cf7ec65f64b8fba7394c87a40e01
kunjdhari rai <86513367+kdrai007@users.noreply.github.com>
2023-03-23 21:23:31 +0530

    Update Footer.js
    some minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
         <footer>
           <div className="container grid grid-four-column">
             <div className="footer-about">
-              <h3>Thapa Technical</h3>
+              <h3>Kd Rai</h3>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
             </div>
             <div className="footer-subscribe">
